7030b154bade9564ba1e9a7a3abc0ac7fecd1ff1
Prateek Saxena <prtksxna@gmail.com>
2015-09-26 09:54:03 +0530

    Add script to read image directory

---
+++ resources/img_to_csv.py
@@ -0,0 +1,20 @@
+import csv
+from os import listdir
+from os.path import isfile, join
+
+# Get list of files from images/ folder
+mypath = 'images'
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+
+# Split filenames to make our data array
+data = []
+for filename in onlyfiles:
+	trend = filename.split( '_' )[ 0 ]
+        designer = filename.split( '_' )[ 1 ].split( 'at' )[ 0 ]
+	score = 1
+	data.append( [ trend, designer, score, filename ] )
+
+# Write data to file
+csv_out = open( 'trends.csv', 'wb' )
+mywriter = csv.writer( csv_out, quoting=csv.QUOTE_ALL )
+mywriter.writerows( data )
